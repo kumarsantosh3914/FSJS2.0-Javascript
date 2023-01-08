1ff33fa742b8aabe63aa3d6af7b6c7dc27ddf6a9
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-01-08 14:35:11 +0530

    changes made

---
+++ Pratics/function.js
@@ -0,0 +1,98 @@
+// conditions
+let standard = 7;
+
+// if(standard == 1) {
+//     console.log("Room One");
+// }
+// else if(standard == 2) {
+//     console.log('Room Two');
+// }
+// else if(standard == 3) {
+//     console.log("Room Number 3");
+// }
+// else {
+//     console.log("You are Principal");
+// }
+
+
+
+// Array 
+let name = [
+    'Santosh',
+    'Shubham',
+    'Mohit',
+    'Pritam',
+];
+
+// console.log(name);
+// console.log(name[0]);
+
+// Object
+
+let backAccount = {
+    firstName: 'Santosh',
+    lastName: 'Kumar',
+    mobileNo: '969855869056',
+    balance: '898999989',
+};
+
+// console.log(backAccount);
+// console.log(backAccount.firstName);
+
+// Date and Math
+
+const now = new Date();
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+
+
+// console.log(now.getTime());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+// Math 
+
+const PI = Math;
+// console.log(PI);
+
+// console.log(Math.round(PI));
+// console.log(Math.round(9.4));
+// console.log(Math.floor(9.5));
+// console.log(Math.ceil(9.5));
+
+
+// min and max 
+
+// console.log(Math.min(10, 1, 23, 4, 6));
+// console.log(Math.max(10, 1, 2, 3, 5));
+
+// Random 
+
+// console.log(Math.round(Math.random() * 51)); 
+
+// console.log(Math.pow(3, 3));
+// console.log(Math.log(2));
+// console.log(Math.sin(90));
+
+// Loops 
+// do while, while, for 
+
+// let i = 6;
+// do {
+//     console.log("value of i is:", i);
+//     i++;
+// } while(i <= 5);
+
+// let j = 6;
+// while(j <= 5) {
+//     console.log('value of j is:', j);
+//     j++;
+// }
+
+for(let i=0; i<=10; i++) {
+    console.log(`${i} * ${i} = ${i * i}`);
+}
