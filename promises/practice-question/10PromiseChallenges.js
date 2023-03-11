49ac701f3279b139c1381733412b4bdf11c22d28
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-03-12 00:11:03 +0530

    code added

--- promises/practice-question/10PromiseChallenges.js
+++ promises/practice-question/10PromiseChallenges.js
@@ -142,3 +142,15 @@ const timer3 = setTimeout(() => {
 }, 0);
 
 console.log('end');
+
+const timer1 = setTimeout(() => {
+    console.log('timer1');
+
+    const promise1 = Promise.resolve().then(() => {
+        console.log('promise1')
+    })
+}, 0)
+
+const timer2 = setTimeout(() => {
+    console.log('timer2')
+}, 0)
